594d41637b250141eae1af4dc4b1bda6d606c641
ferxi14 <fernandogr0309@gmail.com>
2025-10-07 09:23:11 +0200

    Estructuracion

---
+++ U2/programas_02/programa01.py
@@ -0,0 +1,5 @@
+"""
+Escribe un programa que muestre una lista de números del 1 al 10. Resuelve el ejercicio
+de dos formas distintas, utilizando los bucles for y while. Cuando utilices el bucle for
+puedes hacer uso de la función range.
+"""
